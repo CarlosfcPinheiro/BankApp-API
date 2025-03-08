4c911c67cf7e19d7ece090de2938f4f351800c2a
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-03-08 14:31:06 -0300

    feat: Add exception handlers

---
+++ src/main/java/me/dio/api_rest_springboot/domain/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package me.dio.api_rest_springboot.domain.controller.exception;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+import java.util.logging.Logger;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Creating exceptions
+    // take error with IllegalArgumentException
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handle(IllegalArgumentException businessException){
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    // take error with NoSuchElementException
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handle(NoSuchElementException notFoundException){
+        return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
+    }
+
+    // take any error that is not mapped
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handle(Throwable unexpectedException){
+        String message = "Unexpected server error, see the logs.";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
