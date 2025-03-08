96b78c1778ff4fea67ae901df993caf8e2048185
CarlosfcPinheiro <carlosfcpinheiro82@gmail.com>
2025-03-08 13:42:38 -0300

    feat: Add Controller api layer

--- src/main/java/me/dio/api_rest_springboot/domain/controller/UserController.java
+++ src/main/java/me/dio/api_rest_springboot/domain/controller/UserController.java
@@ -1,4 +1,36 @@
 package me.dio.api_rest_springboot.domain.controller;
 
+import me.dio.api_rest_springboot.domain.service.UserService;
+import me.dio.api_rest_springboot.domain.model.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user){
+        User newUser = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(newUser);
+    }
 }
